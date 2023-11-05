156a6342386df9c5c1a723895dcdfa2b22b44b06
Sho.I1022 <119638592+SHONOSUKE100@users.noreply.github.com>
2023-11-05 12:50:41 +0900

    register PostgresSQL on dev environment

--- backend/config/settings/dev.py
+++ backend/config/settings/dev.py
@@ -10,10 +10,13 @@ DEBUG = os.environ.get("DEBUG") == "True" #DEBUGはbool型なので文字列の�
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1").split(", ")
 
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get("SQL_DATABASE"),
+        'USER': os.environ.get("SQL_USER"),
+        'PASSWORD': os.environ.get("SQL_PASSWORD"),
+        'HOST': os.environ.get("SQL_HOST"),
+        'PORT': os.environ.get("SQL_PORT"),
     }
 }
