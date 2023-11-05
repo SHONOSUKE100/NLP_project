061417637f6db7a013b760ebc175c457ed4a3bfd
Hayato-T-08 <104160005+Hayato-T-08@users.noreply.github.com>
2023-11-05 21:52:39 +0900

    add model select button

--- frontend/src/components/Text_display.vue
+++ frontend/src/components/Text_display.vue
@@ -2,6 +2,27 @@
 <template>
   <v-main class="">
     <v-container>
+      <div class="text-center">
+        
+        <v-menu>
+          <template v-slot:activator="{ props }">
+            <v-btn color="primary" v-bind="props">
+              Model
+            </v-btn>
+          </template>
+          <v-list @click:select="clickItem">
+            <v-list-item key="1" value="1">
+              <v-list-item-title>Model1</v-list-item-title>
+            </v-list-item>
+            <v-list-item key="2" value="2">
+              <v-list-item-title>Model2</v-list-item-title>
+            </v-list-item>
+            <v-list-item key="3" value="3">
+              <v-list-item-title>Model3</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </div>
       <div v-if="result">
         <span v-for="(item, index) in result.text" :key="index">
           <span v-if="item.label === 'None'">
@@ -19,14 +40,24 @@
 <script>
 import axios from 'axios';
 import Dialog from './Dialog.vue';
-
+import { ref } from 'vue';
 
 export default {
   name: 'TextDisplay',
+  setup() {
+    const url = ref('http://localhost:8000/api/get_processed_text/');
+  },
   data() {
     return {
       result: null,
       showModal: false,
+      selected: '',
+      url: "http://localhost:8000/api/get_processed_text/",
+      endpoints: [
+        {id: 1, name: 'model1',url: "http://localhost:8000/api/get_processed_text/"},
+        {id: 2, name: 'model2', url: "aaaa"},
+        {id: 3, name: 'model3', url: "a"},
+      ]
     };
   },
   components: {
@@ -48,6 +79,9 @@ export default {
           console.log('Error Headers:', error.response.headers);
         });
     },
+    clickItem(event){
+      this.selected = event.id;
+    }
   },
   mounted() {
     this.fetchResult();
