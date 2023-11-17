dab6b9fe7be51dd97b713faeb5883407d7ba6cd8
Sho.I1022 <119638592+SHONOSUKE100@users.noreply.github.com>
2023-11-17 23:56:52 +0900

    fix

--- backend/config/settings/dev.py
+++ backend/config/settings/dev.py
@@ -4,7 +4,7 @@ import os
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
-print(SECRET_KEY)
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG") == "True" #DEBUGはbool型なので文字列の判定結果を格納する
 
